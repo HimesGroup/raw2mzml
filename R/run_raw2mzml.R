0b8b5d9c27d6bfb96b37843daa8e596d857a2504
Jaehyun Joo <jaehyunjoo@outlook.com>
2023-01-11 20:53:33 -0500

    Made web browser launches in non-interactive mode after the app started

--- R/run_raw2mzml.R
+++ R/run_raw2mzml.R
@@ -1,7 +1,7 @@
 ##' @export
 run_raw2mzml <- function() {
   if (.Platform$OS.type == "windows") {
-    shinyApp(ui, server)
+    shinyApp(ui, server, options = list(launch.browser = TRUE))
   } else {
     stop("This app requires Windows OS")
   }
